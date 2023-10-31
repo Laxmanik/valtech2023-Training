dd71dc76f828ef5c52548a93d86dca7b1c56505f
Laxman Kuddemmi <laxmankuddemmi@gmail.com>
2023-10-31 14:26:47 +0530

    Updated App.java and App.test

--- first-maven-project/src/test/java/com/valtech/training/first_maven_project/AppTest.java
+++ first-maven-project/src/test/java/com/valtech/training/first_maven_project/AppTest.java
@@ -35,4 +35,9 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    public void testSayHello() {
+    	App app = new App();
+    	assertEquals("Hello World!", app.sayHello());
+    }
 }
